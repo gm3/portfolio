e699dc5c20af9bfea613004f74c8bbba8b25401d
gm3 <7612104+gm3@users.noreply.github.com>
2025-08-13 12:47:05 -0500

    add /docs

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/portfolio/',
   // assetsInclude: ['**/*.glb'], // removed lanyard/vrm assets for now
+  build: { outDir: 'docs' },
   plugins: [react()],
 })
 
